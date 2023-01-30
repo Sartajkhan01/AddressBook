519e4fea8283a1edc300c67df369bf6d214e34ae
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-30 14:48:19 +0530

    [Sartaj] : added Ristructing duplicate contact program

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,40 +5,58 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Adress Book program..... ");
-            
-            AddressBookMain createContact = new AddressBookMain();
+            Console.WriteLine("Welcome To The Address Book Program\n===================================");
+            AddressBookMain addingContact = new AddressBookMain();
             bool end = true;
-            while (true)
+            while (end)
             {
-                Console.WriteLine("Choose an option to execute the program : \n1. Add a contact\n2. Display contact\n3." +
-                    " Edit a contact\n4. Delete a contact\n5. End the program");
+                Console.WriteLine("Choose an option to execute :\n1. Create Contact\n2. Add Contact\n3. Edit Contact\n4." +
+                    " Delete Contact\n5. View Contact\n6. Restricting Duplicate Contact\n7. End The Program");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        createContact.CreateContact();
+                        addingContact.AddContact();
+                        Console.WriteLine("Contact has created");
                         break;
                     case 2:
-                        createContact.Display();
+                        addingContact.AddContact();
+                        Console.WriteLine("Contact has added ");
+                        addingContact.Display();
                         break;
                     case 3:
-                        createContact.EditContact();
+                        addingContact.EditContact();
                         break;
                     case 4:
-                        createContact.DeleteContact();
+                        addingContact.DeleteContact();
                         break;
                     case 5:
-                        createContact.ViewContact();
+                        addingContact.ViewContact();
                         break;
                     case 6:
+                        MultipleAddressBook studentAddressBook = new MultipleAddressBook();
+                        studentAddressBook.AddDetails("Engineer", "Sartaj", "Khan", "Whitefield", "Nagpur", "Maharashtra", 4400002, 9424777313, "sartajikhan786@gmail.com");
+                        studentAddressBook.AddDetails("Engineer", "Abhishek", "Jadhav", "Bandra", "Mumbai", "Maharashtra", 560464, 1236547899, "sartajikhan786@gmail.com");
+                        studentAddressBook.AddDetails("Engineer", "Shivraj", "Gawda", "Malleshwaram", "Bengaluru", "karnataka", 560066, 9424464313, "sartajikhan786@gmail.com");
+                        studentAddressBook.AddDetails("Engineer", "Rajesh", "Singh", "GTNagar", "pune", "Maharashtra", 564566, 9433777313, "sartajikhan786@gmail.com");
+                        studentAddressBook.AddDetails("Engineer", "Vivek", "Singh", "HSR", "Bengaluru", "karnataka", 560996, 9424777315, "sartajikhan786@gmail.com");
+                        studentAddressBook.AddDetails("Engineer", "Siddhant", "Singh", "City Centre", "Noida", "U.P.", 458963, 9425477313, "sartajikhan786@gmail.com");
+                        Console.WriteLine("Enter Engineer Address Book Name : ");
+                        string addressBook = Console.ReadLine();
+                        studentAddressBook.AddressBook(addressBook);
+                        studentAddressBook.ComputeDetails();
+                        studentAddressBook.Search();
+                        studentAddressBook.Count();
+                        break;
+                    case 7:
                         end = false;
                         break;
                     default:
-                        Console.WriteLine("Incorrect Option ");
+                        Console.WriteLine("Invalid option ");
                         break;
                 }
             }
+
         }
     }
 }
